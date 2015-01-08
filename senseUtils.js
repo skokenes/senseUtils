7055e7c757c54e82cbdd61e0560b13c7f7354935
skokenes <speroskokenes@gmail.com>
2015-01-08 18:23:27 -0500

    New layout functions; bug fix
    Bug: issue with the array prototype extend function being enumerable in
    the array; fixed with alternate implementation method

--- senseUtils.js
+++ senseUtils.js
@@ -323,11 +323,70 @@ var senseUtils = {
             callback($el, layout, bigMatrix, me);
             //(DOM element, layout object, new flattened matrix, this)
         }
+    },
+    getMeasureLabel: function(n,layout) {
+        return layout.qHyperCube.qMeasureInfo[n-1].qFallbackTitle;
+    },
+
+    getDimLabel: function(n,layout) {
+        return layout.qHyperCube.qDimensionInfo[n-1].qFallbackTitle;
+    },
+    setupContainer: function($element,layout,class_name) {
+        // Properties: height, width, id
+        var ext_height = $element.height(),
+            ext_width = $element.width(), 
+            id = "ext_" + layout.qInfo.qId;
+
+        // Initialize or clear out the container and its classes
+        if (!document.getElementById(id)) {
+            $element.append($("<div />").attr("id",id));
+        }
+
+        else {
+        
+            $("#" + id)
+                .empty()
+                .removeClass();
+
+        }
+
+        // Set the containers properties like width, height, and class
+        
+        $("#" + id)
+            .width(ext_width)
+            .height(ext_height)
+            .addClass(class_name);
+
+        return id;
+    },
+    extendLayout: function(layout,self) {
+        var dim_count = layout.qHyperCube.qDimensionInfo.length;
+
+        layout.qHyperCube.qDataPages[0].qMatrix.forEach(function(d) {
+            d.dim = function(i) {
+                return d[i-1];
+            };
+            d.measure = function(i) {
+                return d[i+dim_count-1];
+            };
+
+            for (var i = 0; i<dim_count; i++) {
+                d[i].qSelf = self;
+                d[i].qIndex = i;
+                d[i].qSelect = function() {
+                    this.qSelf.backendApi.selectValues(this.qIndex,[this.qElemNumber],true);    
+                };
+                        
+            };
+        });
     }
 };
-//function to append arrays to other arrays
-Array.prototype.extend = function(arr) {
-    arr.forEach(function(v) {
-        this.push(v)
-    }, this);
-}
+
+Object.defineProperty(Array.prototype, 'extend', {
+    enumerable: false,  
+    value:function(arr) {
+        arr.forEach(function(v) {
+            this.push(v);
+        },this);
+    }
+});
